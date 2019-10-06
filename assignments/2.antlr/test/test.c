74389b6939a62886bd40c58357aeaa96f3fbec70
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-10-07 00:42:17 +0900

    submitted assignment

---
+++ assignments/2.antlr/test/test.c
@@ -0,0 +1,21 @@
+void func_test()
+{
+
+}
+
+int main()
+{
+    int a;
+    int b[2];
+
+    a = 3 + 2;
+    b = a + 4;
+
+    func_test();
+
+    if ( a >= b ) {
+        while (a == b) {
+
+        }
+    }
+}

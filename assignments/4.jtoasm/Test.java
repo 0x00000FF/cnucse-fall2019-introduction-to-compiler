fe15f7ba9706fb5aac8ad0219b3d001e88d5cf79
Kangjun Heo == P.Knowledge <knowledge@patche.me>
2019-10-17 15:44:26 +0900

    update some optimizations

--- assignments/4.jtoasm/Test.java
+++ assignments/4.jtoasm/Test.java
@@ -30,9 +30,9 @@ public class Test extends java.lang.Object {
     // Method : add [Static]
     // Purpose: This method adds two int values and returns.
     // Paramters:
-    //     _a          int
-    //     _b          int
-    // Returns  : int ( _a + _b )
+    //     _l_0x26     int
+    //     _l_0x27     int
+    // Returns  : int 
     // Side-Effects :
     //      This method has no side effects. 
     public static int add(int l_0x26, int l_0x27) {
@@ -52,8 +52,7 @@ public class Test extends java.lang.Object {
     public static void main(String[] _Vmain_LString_0) {
         // ldc 33
         // lstore_2
-        final int rc_0 = 33;
-        int l_0x28 = rc_0;
+        int l_0x28 = 33;
 
         // getstatic java/lang/System/out Ljava/io/PrintStream;
         java.io.PrintStream _ls_Ljava_io_PrintStream_0 = java.lang.System.out;
@@ -65,13 +64,12 @@ public class Test extends java.lang.Object {
         // this invokes static method Test.add
         // with one constant and one variable l_0x28
         // also, it passes its return to next virutal method invocation
-        final int rc_1 = 1;
 
         // invokevirtual java/io/PrintStream/println(I)V
         // ------------------------
         // virtual method println invocation of System.out
         // System.out is stored in _ls_Ljava_io_PrintStream_0
         // prints return of Test.add that just invoked
-        _ls_Ljava_io_PrintStream_0.println( Test.add(rc_1, l_0x28) );
+        _ls_Ljava_io_PrintStream_0.println( Test.add(1, l_0x28) );
     }
 }

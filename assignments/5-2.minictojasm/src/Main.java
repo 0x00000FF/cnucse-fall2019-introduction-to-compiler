cb76091de3d7f2d110c00f4603182f0d23f78672
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-26 15:23:27 +0900

    Body

--- assignments/5-2.minictojasm/src/Main.java
+++ assignments/5-2.minictojasm/src/Main.java
@@ -6,8 +6,49 @@
  *  201704150 Kangjun Heo
  */
 
-public class Main {
-    public static void main(String[] args) {
+import generated.MiniCLexer;
+import generated.MiniCParser;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.stringtemplate.v4.compiler.Bytecode;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class Main {
+    // Remove unnecessary System.out (.NET Style)
+    static PrintStream Console = System.out;
+
+    public static void main(String[] args) {
+        Console.println(
+                "MiniC to Java Bytecode Compiler\n" +
+                "Copyright by 201704150 Kangjun Heo, All rights reserved.\n" +
+                "=========");
+
+        if (args.length < 1) {
+            Console.println("java -jar mcjc.jar <filename>");
+            System.exit(1);
+        }
+
+        try {
+            CharStream cs = CharStreams.fromFileName(args[0]);
+            MiniCLexer lexer = new MiniCLexer(cs);
+            CommonTokenStream tokens = new CommonTokenStream( lexer );
+            MiniCParser parser = new MiniCParser( tokens );
+            ParseTree tree = parser.program();
+
+            ParseTreeWalker walker = new ParseTreeWalker();
+            ParseTreeListener listener = new BytecodeGenListener();
+            walker.walk(listener, tree);
+
+
+        } catch (IOException e) {
+            Console.println("Error while opening designated file.");
+        }
     }
 }
